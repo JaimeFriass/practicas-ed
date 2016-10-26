4946e41f87adda1f6c67dc8e52986ca8c7a6c813
jesusjimsa <jesusjimsa@icloud.com>
2016-10-26 23:23:43 +0200

    Delete

--- Práctica 2/TDAmutacion_v0/Documentación/html/search/all_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['enfermedad',['enfermedad',['../classenfermedad.html',1,'enfermedad'],['../classenfermedad.html#a60eb5e620b0bf9a53d4f0980031aeefd',1,'enfermedad::enfermedad()'],['../classenfermedad.html#a7caef55b00a31ce18191ceaba81ed20c',1,'enfermedad::enfermedad(const string &amp;name, const string &amp;ID, const string &amp;database)']]],
-  ['enfermedad_2eh',['enfermedad.h',['../enfermedad_8h.html',1,'']]],
-  ['enfermedad_2ehxx',['enfermedad.hxx',['../enfermedad_8hxx.html',1,'']]],
-  ['enfermedades',['enfermedades',['../classmutacion.html#a0decaa0406675999e766f913e6347d79',1,'mutacion']]]
-];
