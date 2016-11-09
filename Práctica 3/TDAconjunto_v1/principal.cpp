c3ae50b49c74e234676bbfb9fbbd350249357051
jesusjimsa <jesusjimsa@icloud.com>
2016-11-09 09:47:44 +0100

    Prueba empírica de eficiencia

--- Práctica 3/TDAconjunto_v1/principal.cpp
+++ Práctica 3/TDAconjunto_v1/principal.cpp
@@ -3,9 +3,12 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <algorithm>
 
 
 using namespace std;
+using namespace std::chrono;
 
 /** @brief lee un fichero de mutaciones, linea a linea
 	@param[in] s nombre del fichero
@@ -14,28 +17,39 @@ using namespace std;
 	*/
 
 bool load(conjunto &  cm, const string & s) {
- ifstream fe;
- string cadena;
+	ifstream fe;
+	string cadena;
+	high_resolution_clock::time_point start,end;
+	duration<double> tiempo_transcurrido;
 	
- cout << "Abrimos "<< s << endl;
- fe.open(s.c_str(), ifstream::in);
- if (fe.fail()){
-	 cerr << "Error al abrir el fichero " << s << endl;
- }
- else {
-	 //leo la cabecera del fichero (líneas que comienzan con #)
-	 do{
-		 getline(fe,cadena,'\n');
-	 }while (cadena.find("#")==0 && !fe.eof());
+	cout << "Abrimos "<< s << endl;
+	fe.open(s.c_str(), ifstream::in);
+	if (fe.fail()){
+		cerr << "Error al abrir el fichero " << s << endl;
+	}
+	else {
+		//leo la cabecera del fichero (líneas que comienzan con #)
+		do{
+			getline(fe,cadena,'\n');
+		}while (cadena.find("#")==0 && !fe.eof());
 	 
-	 //leo la tabla de mutaciones, una línea cada vez
-	 while ( !fe.eof() ){
+		//leo la tabla de mutaciones, una línea cada vez
+		while ( !fe.eof() ){
 		 //cout << "leo:: "<< cadena << endl;
 		 // Invoco el constructor de mutación que recibe una cadena completa, la parsea y crea la mutación.
-		 mutacion mut = mutacion(cadena);
+			mutacion mut = mutacion(cadena);
 		 // Insertar mutación en el conjunto
-		 cm.insert(mut);
-		 getline(fe,cadena,'\n');
+		 
+			start =  high_resolution_clock::now();;
+		 
+			cm.insert(mut);
+		 
+			end =  high_resolution_clock::now();;
+			
+			tiempo_transcurrido  = duration_cast<duration<double> >(end-start);
+			cout << "\nTiempo insert : "<< tiempo_transcurrido.count() << endl;
+		 
+			getline(fe,cadena,'\n');
 	 }
 	 fe.close();
 	 return true;
@@ -49,6 +63,8 @@ int main(int argc, char *argv[]){
 	
 	conjunto conjuntoMutaciones;
 	string query_str;
+	high_resolution_clock::time_point start,end;
+	duration<double> tiempo_transcurrido;
 	
 	//Cargar las mutaciones en vectorSNPs
 	load(conjuntoMutaciones, "clinvar_20160831.vcf");
@@ -56,17 +72,45 @@ int main(int argc, char *argv[]){
 	//Imprimir número de elementos almacenados en conjuntoMutaciones
 	cout << "Lectura del fichero finalizada. Mutaciones cargadas: " << conjuntoMutaciones.size() << endl;
 	
+	
+	
 	//Imprimir cuántas mutaciones están asociadas al cromosoma 1:
 	cout << "Mutaciones asociadas a Chr 1: " << conjuntoMutaciones.lower_bound("2", 1) - conjuntoMutaciones.begin() << endl;
 	
+	
 	/**@todo ¿Existe la mutación con ID "rs147165522"? Imprimir la mutación y las enfermedades asociadas */
+	/*
 	cout<< "la mutación y las enfermedades asociadas con ID: rs147165522" << conjuntoMutaciones.find("rs147165522").first << " " << conjuntoMutaciones.find("rs147165522").second << endl;
 	
+	
+	 */
+	
+	
 	/**@todo ¿Existe la mutación en chr/pos "14"/67769578? Imprimir la mutación y las enfermedades asociadas */
+	start =  high_resolution_clock::now();;
+	
 	cout<< "la mutación y las enfermedades asociadas con char:14y pos: 67769578" << conjuntoMutaciones.find("14",67769578).first << conjuntoMutaciones.find("14",67769578).second << endl;
 	
+	end =  high_resolution_clock::now();;
+	
+	tiempo_transcurrido  = duration_cast<duration<double> >(end-start);
+	cout << "\nTiempo find : "<< tiempo_transcurrido.count() << endl;
+	
 	/**@todo ¿Cómo podríamos calcular el número de mutaciones del cromosoma 3? (utiliza lower_bound / upper_bound) */
+	/*
 	cout<< "Número de mutaciones del cromosoma 3: " << conjuntoMutaciones.upper_bound("3",0) - conjuntoMutaciones.begin() << endl;
+	*/
+	
+	//////////////Prueba de erase///////////////////
+	
+	start =  high_resolution_clock::now();;
+	
+	conjuntoMutaciones.erase("rs147165522");
+	
+	end =  high_resolution_clock::now();;
+	
+	tiempo_transcurrido  = duration_cast<duration<double> >(end-start);
+	cout << "\nTiempo erase : "<< tiempo_transcurrido.count() << endl;
 	
 	/**@todo Analiza la eficiencia teórica y empírica de las operaciones find, insert y erase */
 	
