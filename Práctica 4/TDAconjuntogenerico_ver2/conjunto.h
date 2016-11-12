a84b77703af6f27e3b1384f0afaf2d3f3087e078
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2016-11-12 19:21:40 +0100

    Repartir trabajo

--- Práctica 4/TDAconjuntogenerico_ver2/conjunto.h
+++ Práctica 4/TDAconjuntogenerico_ver2/conjunto.h
@@ -11,23 +11,24 @@
 #include <iostream>
 
 #include "mutacion.h"
+#include "enfermedad.h"
 
 using namespace std;
 
 
 //! Clase conjunto
 /*! conjunto<T,CMP>::conjunto, find, size,  insert, erase,
- 
+
  * Tipos conjunto<T,CMP>::value_type, conjunto<T,CMP>::size_type, conjunto<T,CMP>::iterator, conjunto<T,CMP>::const_iterator
- 
+
  * Descripción
- 
+
  * Un conjunto es un contenedor que permite almacenar en orden creciente un conjunto de elementos no repetidos.  En nuestro caso el conjunto va a tener un subconjunto restringido de métodos (inserción de elementos, consulta de un elemento, etc). Este conjunto "simulará" un conjunto de la stl, con algunas claras diferencias pues, entre otros,  no estará dotado de la capacidad de iterar (recorrer) a través de sus elementos.
- 
+
  * Asociado al conjunto, tendremos el tipo  \code conjunto::value_type \endcode que permite hacer referencia al elemento  almacenados en cada una de las posiciones del conjunto, en nuestro caso mutaciones (SNPs) o enfermedades. Es requisito que el tipo conjunto::value_type tenga definida una relación de orden, CMP, y el operador de asignación, operator= .
- 
+
  * El número de elementos en el conjunto puede variar dinámicamente; la gestión de la memoria es automática.
- 
+
  @todo Implementa esta clase siguiendo la especificación asociada
  */
 
@@ -40,40 +41,40 @@ public:
 	typedef unsigned int size_type;
 	typedef vector<value_type>::iterator iterator;
 	typedef vector<value_type>::const_iterator const_iterator;
-	
-	//1
+
+	//Nazaret
 	/** @brief constructor primitivo.
 	 */
 	conjunto( );
-	
-	//2
+
+	//Jaime
 	/** @brief constructor de copia
 	 @param[in] d conjunto a copiar
 	 */
 	conjunto (const conjunto<T,CMP> & d);
-	
-	//3
+
+	//Jesús
 	/** @brief busca una entrada en el conjunto
-	 @param[in] s: entrada  a buscar.
+	 @param[in] s entrada  a buscar.
 	 @return Si existe una entrada en el conjunto con ese valor devuelve el iterador a su posicion, en caso contrario devuelve iterador al final de conjunto
 	 @post no modifica el conjunto.
-	 
+
 	 */
-	iterator  find (const value_type & s) ;
+	iterator  find (const value_type & s);
 	const_iterator  find (const value_type & s) const;
-	
-	
-	//1
+
+
+	//Nazaret
 	/** @brief cuenta cuantas entradas coinciden con los parámetros dados.
 	 @param[in] e entrada.
 	 @return Como el conjunto no puede tener entradas repetidas, devuelve 1 (si se encuentra la entrada) o 0 (si no se encuentra).
 	 @post no modifica el conjunto.
 	 */
-	
+
 	size_type count (const value_type & e) const;
-	
-	
-	//2
+
+
+	//Jaime
 	/** @brief Inserta una entrada en el conjunto
 	 @param val entrada a insertar
 	 @return un par donde el segundo campo vale true si la entrada se ha podido insertar con éxito, esto es, no existe una mutación con igual valor en el conjunto. False en caso contrario.
@@ -82,75 +83,75 @@ public:
 	 */
 	pair<iterator,bool> insert (const value_type& val);
 	pair<iterator,bool> insert (value_type& val);
-	
-	//3
+
+	//Jesús
 	/** @brief Borra una entrada en el conjunto .
 	 Busca la entrada y si la encuentra la borra.
-	 
 	 @param[in] val entrada a borrar.
-	 @param[in] position itarador que apunta a la  entrada que geremos borrar
+	 @param[in] position itarador que apunta a la  entrada que queremos borrar
 	 @return devuelve la posicion siguiente al elemento borrado (para la version con iterador) o el numero de elementos borrados
 	 @post Si esta en el conjunto su tamaño se decrementa en 1.
 	 */
 	iterator  erase (const iterator position);
 	size_type erase (const value_type& val);
-	
-	//1
+
+	//Nazaret
 	/** @brief Borra todas las entradas del conjunto, dejandolo vacio.
 	 @post El conjunto se modifica, quedando vacio.
 	 */
 	void clear();
-	
-	//2
+
+	//Jaime
 	/** @brief numero de entradas en el conjunto
 	 @post  No se modifica el conjunto.
 	 @return numero de entradas en el conjunto
 	 */
 	size_type size() const ;
-	
-	//3
+
+	//Jesús
 	/** @brief Chequea si el conjunto esta vacio (size()==0)
 	 @post  No se modifica el conjunto.
 	 */
 	bool empty() const;
-	
-	//1
+
+	//Nazaret
 	/** @brief operador de asignación
 	 @param[in] org conjunto a copiar.
 	 @return Crea y devuelve un conjunto duplicado exacto de org.
 	 */
 	conjunto & operator=( const conjunto & org);
-	
-	//2
+
+	//Jaime
 	/** @brief begin del conjunto
 	 @return Devuelve un iterador al primer elemento del conjunto. Si no existe devuelve end
 	 @post no modifica el conjunto.
 	 */
 	conjunto::iterator begin ();
-	
-	//3
+
+	//Jesús
 	/** @brief end del conjunto
 	 @return Devuelve un iterador al final del conjunto (posicion siguiente al ultimo).
 	 @post no modifica el conjunto.
 	 */
 	conjunto::iterator end ();
-	
-	//1
+
+	//Nazaret
 	/** @brief begin del conjunto
 	 @return Devuelve un iterador constante al primer elemento del conjunto. Si no existe devuelve cend
 	 @post no modifica el conjunto.
 	 */
 	conjunto::const_iterator cbegin () const;
-	
-	//2
+
+	//Jaime
 	/** @brief end del conjunto
 	 @return Devuelve un iterador constante al final del conjunto (posicion siguiente al ultimo.
 	 @post no modifica el conjunto.
 	 */
 	conjunto::const_iterator cend () const;
-	
-	//3
-	/** @brief busca primer elemento que no está por debajo ('antes', '<') de los parámetros dados.
+
+	//Jesús
+	/**
+	 @brief busca primer elemento que no está por debajo ('antes', '<') de los parámetros dados.
 	 @param[in] val entrada.
 	 @return Devuelve un iterador al primer elemento que cumple que "elemento<e" es falso, esto es, el primer elemento que  es mayor o igual que val
 	 Si no existe devuelve end
@@ -158,8 +159,8 @@ public:
 	 */
 	iterator lower_bound (const value_type& val);
 	const_iterator lower_bound (const value_type& val) const;
-	
-	
+
+	//Nazaret
 	/** @brief busca primer elemento por encima ('después', '>') de los parámetros dados.
 	 @param[in] val entrada. Devuelve un iterador al primer elemento que cumple que "elemento>e", esto es, el primer elemento ESTRICTAMENTE mayor que val
 	 Si no existe devuelve end
@@ -167,35 +168,34 @@ public:
 	 */
 	iterator upper_bound (const value_type& val);
 	const_iterator upper_bound (const value_type& val) const;
-	
-	
-	
+
+
+
 private:
 	vector<value_type> vm; // vector que almacena los elementos del conjunto
-	
-	//1
+
+	//Jaime
 	CMP comp; // criterio para guiar la ordenacion
-	
-	
-	
+
+
+
 	/** @brief Chequea el Invariante de la representacion
 	 @return true si el invariante es correcto, falso en caso contrario
 	 */
 	bool cheq_rep( ) const;
-	
-	
- 
-	
+
+
+
+
 };
 
+template <typename T, typename CMP>
 
+//Jesús
 /** @brief imprime todas las entradas del conjunto
  @post No se modifica el conjunto.
  Implementar tambien esta funcion
-	*/
-template <typename T, typename CMP>
-
-//2
+*/
 ostream &  operator << ( ostream & sal, const conjunto<T,CMP> & C);
 
 
