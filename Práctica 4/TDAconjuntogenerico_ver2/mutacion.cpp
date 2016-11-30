25081026cb7ea6f276ecfbed8db514f0093ba1eb
jesusjimsa <jesusjimsa@icloud.com>
2016-11-30 14:42:11 +0100

    En busca del error perdido

--- Práctica 4/TDAconjuntogenerico_ver2/mutacion.cpp
+++ Práctica 4/TDAconjuntogenerico_ver2/mutacion.cpp
@@ -35,14 +35,14 @@ mutacion::mutacion(const string & str){
 	cerr << "Invocado constructor mutacion::mutacion(str)" << endl;
 	// OBTENER CHR
 	(*this).chr = str.substr(0,1);
-	cerr<< "\tchr: "<<(*this).chr<<endl; 
+	//cerr<< "\tchr: "<<(*this).chr<<endl; 
 	
 	// OBTENER POS
 	// En el substring que va desde el primer dígito de pos hasta rs, busca el
 	// tabulador que marca el final de pos.
 	string aux = str.substr(2,str.substr(2,str.find("rs")).find('\t'));
 	(*this).pos = atoi(aux.c_str());
-	cerr <<"\tpos: "<<(*this).pos<<endl;
+	//cerr <<"\tpos: "<<(*this).pos<<endl;
 	
 	// OBTENER ID
 	i = str.find("rs");
@@ -52,7 +52,7 @@ mutacion::mutacion(const string & str){
 		(*this).ID += str[i];
 		i++;
 	}
-	cerr << "\tID: "<<(*this).ID<<endl;
+	//cerr << "\tID: "<<(*this).ID<<endl;
 
 	// OBTENER REF_ALT
 	i++;
@@ -72,12 +72,12 @@ mutacion::mutacion(const string & str){
 		i++;
 	}
 	(*this).ref_alt.push_back(aux);
-	
-	cerr << "\tREF_ALT: "; 
+	/*
+	//cerr << "\tREF_ALT: "; 
 	for (int i = 0; i < ref_alt.size(); i++)
 		cerr << ref_alt.at(i) << " ";
-	cerr << endl; 
-	
+	//cerr << endl; 
+	*/
 	// OBTENER GENES
 	i = str.find("GENEINFO");
 
@@ -98,11 +98,12 @@ mutacion::mutacion(const string & str){
 			(*this).genes.push_back(aux.substr(j,aux.size() - 1));
 		else
 			(*this).genes.push_back(aux);
-
-		cerr << "\tGENES: "; 
+			/*
+		//cerr << "\tGENES: "; 
 		for (int i=0; i<genes.size(); i++)
 			cerr << genes.at(i) << " ";
-		cerr << endl;
+		//cerr << endl;
+		*/
 	}
 	else{
 		(*this).genes.push_back("");
@@ -118,7 +119,7 @@ mutacion::mutacion(const string & str){
 	else
 		(*this).common = false;
 
-	cerr << "\tCOMMON: " << common << endl;
+	//cerr << "\tCOMMON: " << common << endl;
 	
 	// OBTENER CAF
 	j = 0;
@@ -145,12 +146,12 @@ mutacion::mutacion(const string & str){
 	else{
 		(*this).caf.push_back(0.0);
 	}
-	cerr << "\tCAF: "; 
-	
+	//cerr << "\tCAF: "; 
+	/*
 	for (int i=0; i<caf.size(); i++)
 		cerr << caf.at(i) << " ";
 	cerr << endl; 
-		
+		*/
 
 
 
@@ -166,16 +167,16 @@ mutacion::mutacion(const string & str){
 	if(posName != -1){
 		sigPyC = str.substr(posName, str.size() - posName).find(";");
 		string nomEnf = str.substr(posName + 7, sigPyC - 7);
-		cerr << "Si 1" << endl;
+		//cerr << "Si 1" << endl;
 
 		sigPyC = str.substr(posID, str.size() - posID).find(";");
 		string iDenf = str.substr(posID + 10, sigPyC - 10);
-		cerr << "Si 2" << endl;
+		//cerr << "Si 2" << endl;
 
 		sigPyC = str.substr(posDB, str.size() - posDB).find(";");
 		string dBenf = str.substr(posDB + 8, sigPyC - 8);
 		vector<string> auxNombre, auxID, auxDB;
-		cerr << "Si 3" << endl;
+		//cerr << "Si 3" << endl;
 
 		int numero_enf = 0;
 		
@@ -189,7 +190,7 @@ mutacion::mutacion(const string & str){
 			}
 		}
 		
-		cerr << "Si 4" << endl;
+		//cerr << "Si 4" << endl;
 		
 		if(extras){
 			auxNombre.push_back(nomEnf.substr(j, nomEnf.size()-j));
@@ -199,7 +200,7 @@ mutacion::mutacion(const string & str){
 			auxNombre.push_back(nomEnf);
 		}
 		
-		cerr << "Si 5" << endl;
+		//cerr << "Si 5" << endl;
 		
 		j = 0;
 		extras = false;
@@ -216,7 +217,7 @@ mutacion::mutacion(const string & str){
 			}
 		}
 		
-		cerr << "Si 6" << endl;
+		//cerr << "Si 6" << endl;
 		
 		if(extras){
 			auxID.push_back(iDenf.substr(j, nomEnf.size() - j));
@@ -227,7 +228,7 @@ mutacion::mutacion(const string & str){
 			numero_ID = 1;
 		}
 		
-		cerr << "Si 7" << endl;
+		//cerr << "Si 7" << endl;
 		
 		j = 0;
 		extras = false;
@@ -244,7 +245,7 @@ mutacion::mutacion(const string & str){
 			}
 		}
 		
-		cerr << "Si 8" << endl;
+		//cerr << "Si 8" << endl;
 		
 		if(extras){
 			auxDB.push_back(dBenf.substr(j, nomEnf.size() - j));
@@ -255,11 +256,11 @@ mutacion::mutacion(const string & str){
 			numero_CLNDSDB = 1;
 		}
 		
-		cerr << "Si 9" << endl;
+		//cerr << "Si 9" << endl;
 		
 		j = 0;
 		extras = false;
-		
+		/*
 		for(int w = 0; w < auxNombre.size(); w++){
 			cerr << "AuxNombre " << w << " vale: " << auxNombre[w] << endl;
 		}
@@ -269,7 +270,7 @@ mutacion::mutacion(const string & str){
 		for(int w = 0; w < auxDB.size(); w++){
 			cerr << "AuxDB " << w << " vale: " << auxDB[w] << endl;
 		}
-		
+		*/
 		int iteraciones = 0;
 
 		for(k = 0; k < auxNombre.size(); k++){
@@ -306,16 +307,16 @@ mutacion::mutacion(const string & str){
 			
 			iteraciones++;
 			
-			cerr << "Enfermedad " << k << ": " << auxEnf << endl;
+			//cerr << "Enfermedad " << k << ": " << auxEnf << endl;
 			(*this).enfermedades.push_back(auxEnf);
 		}
-		cerr << "Si 10" << endl;
+		//cerr << "Si 10" << endl;
 
 		sigPyC = str.substr(posCln, str.size() - posCln).find(";");
 		string strCln = str.substr(posCln + 7, sigPyC - 7);
-		cerr << "Si 11" << endl;
+		//cerr << "Si 11" << endl;
 		
-		cerr << "\tIdentificando CLNSIG: cadena: " << strCln << endl;
+		//cerr << "\tIdentificando CLNSIG: cadena: " << strCln << endl;
 		
 		if(strCln.size() >= 1){
 			int k = 0;
@@ -325,7 +326,7 @@ mutacion::mutacion(const string & str){
 				if(strCln[k] != '|' && strCln[k] != ','){
 					(*this).clnsig.push_back((int)strCln[k] - '0');
 					
-					cerr << "\t\tSe ha hecho push_back sobre clnsig de la cadena : " << strCln[k] - '0' << endl;		//strCln[k] - '0' para convertir el string en entero
+					//cerr << "\t\tSe ha hecho push_back sobre clnsig de la cadena : " << strCln[k] - '0' << endl;		//strCln[k] - '0' para convertir el string en entero
 					
 					k++;
 				}
@@ -335,7 +336,7 @@ mutacion::mutacion(const string & str){
 			}
 		}
 	}
-
+	/*
 	cerr << "\tEnfermedades (" << enfermedades.size() << "): ";
 	for (int i = 0; i < enfermedades.size(); i++)
 		cerr << enfermedades.at(i) << " ";
@@ -345,8 +346,7 @@ mutacion::mutacion(const string & str){
 	for (int i = 0; i < clnsig.size(); i++)
 		cerr << clnsig.at(i) << " ";
 	cerr << endl; 
-
-
+*/
 }
 
 //Métodos consultores
